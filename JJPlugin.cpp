687753c647872aedba4e8ae9805b46cf10324216
Jackie <13265466114@163.com>
2018-08-23 23:14:28 +0800

    refactor fumctionality

--- JJPlugin.cpp
+++ JJPlugin.cpp
@@ -17,8 +17,10 @@ using namespace std;
 using namespace llvm;
 using namespace clang::ast_matchers;
 
+
 namespace JJPlugin {
     
+    // MARK: - my handler
     class JJHandler : public MatchFinder::MatchCallback {
     private:
         CompilerInstance &ci;
@@ -26,61 +28,6 @@ namespace JJPlugin {
         
     public:
         JJHandler(CompilerInstance &ci) :ci(ci) {}
-        
-    private:
-        bool JJVisitObjCMethodDecl(const ObjCMethodDecl *declaration)
-        {
-            cout << "JJVisitObjCMethodDecl -- JJVisitObjCMethodDecl -- JJVisitObjCMethodDecl " << endl;
-            if (isUserSourceCode(declaration))
-            {
-                checkMethodNameForUppercaseName(declaration);
-                cout << "isUserSourceCode -- true" << endl;
-//                checkMethodParamsNameForUppercaseName(declaration);
-//                checkMethodBodyForOver500Lines(declaration);
-            }
-            
-            return true;
-        }
-        
-        bool VisitStmt(Stmt *s)
-        {
-            string filename = context->getSourceManager().getFilename(s->getSourceRange().getBegin()).str();
-            
-            if (filename.empty())
-                cout << "filename.empty() -- filename.empty()" << endl;
-            return false;
-            
-            if(filename.find("/Applications/Xcode.app/") == 0)
-                cout << "filename.find(\"/Applications/Xcode.app/\")" << endl;
-            return false;
-            
-            return true;
-        }
-        
-        //判断是否为用户源码
-        //@return true 为用户源码，false 非用户源码
-        bool isUserSourceCode (const Decl *decl)
-        {
-//            cout << "isUserSourceCode -- isUserSourceCode" << endl;
-//            string filename = context->getSourceManager().getFilename(decl->getSourceRange().getBegin()).str();
-            
-//            string filename = ci.getSourceManager().getFilename(decl->getSourceRange().getBegin()).str();
-//            StringRef name = ci.getSourceManager().getFilename(decl->getSourceRange().getBegin());
-            
-//            cout << "filename: -- " + filename << endl;
-//            if (name.empty())
-//                cout << "filename.empty() -- filename.empty()" << endl;
-//                return false;
-////
-            //非XCode中的源码都认为是用户源码
-//            if(name.find("/Applications/Xcode.app/") == 0)
-//                cout << "filename.find(\"/Applications/Xcode.app/\")" << endl;
-//                return false;
-
-            cout << "isUserSourceCode true -- true --true" << endl;
-            return true;
-        }
-        
         
         //检测方法名是否存在大写开头
         void checkMethodNameForUppercaseName(const ObjCMethodDecl *decl)
@@ -113,53 +60,115 @@ namespace JJPlugin {
             }
         }
         
+        void checkPropertyDecl(const clang::ObjCPropertyDecl *propertyDecl)
+        {
+            ObjCPropertyDecl::PropertyAttributeKind attrKind = propertyDecl->getPropertyAttributes();
+            SourceLocation location = propertyDecl->getLocation();
+            string typeStr = propertyDecl->getType().getAsString();
+            
+            
+            if (propertyDecl->getTypeSourceInfo()) {
+                if(!(attrKind & ObjCPropertyDecl::OBJC_PR_nonatomic)){
+                    diagWaringReport(location, "Are you sure to use atomic which might reduce the performance.", NULL);
+                }
+                
+                if ((typeStr.find("NSString")!=string::npos)&& !(attrKind & ObjCPropertyDecl::OBJC_PR_copy)) {
+                    diagWaringReport(location, "NSString 应该使用 copy 代替 strong.", NULL);
+                } else if ((typeStr.find("NSArray")!=string::npos)&& !(attrKind & ObjCPropertyDecl::OBJC_PR_copy)) {
+                    diagWaringReport(location, "NSArray 应该使用 copy 代替 strong.", NULL);
+                }
+                
+                if(!typeStr.compare("int")){
+                    diagWaringReport(location, "Use the built-in NSInteger instead of int.", NULL);
+                } else if ((typeStr.find("<")!=string::npos && typeStr.find(">")!=string::npos) && !(attrKind & ObjCPropertyDecl::OBJC_PR_weak)) {
+                    diagWaringReport(location, "Use weak declare Delegate.", NULL);
+                }
+            }
+        }
+        
+        void checkInterfaceDecl(const ObjCInterfaceDecl *decl){
+            StringRef className = decl->getName();
+            
+            char c = className[0];
+            if (isLowercase(c)) {
+                std::string tempName = className;
+                cout << tempName << endl;
+                tempName[0] = toUppercase(c);
+                StringRef replacement(tempName);
+                SourceLocation nameStart = decl->getLocation();
+                SourceLocation nameEnd = nameStart.getLocWithOffset(className.size() - 1);
+                FixItHint fixItHint = FixItHint::CreateReplacement(SourceRange(nameStart, nameEnd), replacement);
+                
+                DiagnosticsEngine &D = ci.getDiagnostics();
+                int diagID = D.getCustomDiagID(DiagnosticsEngine::Error, "老子警告你：类名不能以小写字母开头");
+                SourceLocation location = decl->getLocation();
+                D.Report(location, diagID).AddFixItHint(fixItHint);
+            }
+            
+            size_t pos = decl->getName().find('_');
+            if (pos != StringRef::npos) {
+                DiagnosticsEngine &D = ci.getDiagnostics();
+                SourceLocation loc = decl->getLocation().getLocWithOffset(pos);
+                
+                std::string tempName = className;
+                std::string::iterator end_pos = std::remove(tempName.begin(), tempName.end(), '_');
+                tempName.erase(end_pos, tempName.end());
+                StringRef replacement(tempName);
+                SourceLocation nameStart = decl->getLocation();
+                SourceLocation nameEnd = nameStart.getLocWithOffset(className.size() - 1);
+                FixItHint fixItHint = FixItHint::CreateReplacement(SourceRange(nameStart, nameEnd), replacement);
+                
+                int diagID = D.getCustomDiagID(DiagnosticsEngine::Error, "老子警告你：类名中不能带有下划线");
+                D.Report(loc, diagID).AddFixItHint(fixItHint);
+            }
+        }
+        
+        template <unsigned N>
+        void diagWaringReport(SourceLocation Loc, const char (&FormatString)[N], FixItHint *Hint)
+        {
+            DiagnosticsEngine &diagEngine = ci.getDiagnostics();
+            unsigned DiagID = diagEngine.getCustomDiagID(clang::DiagnosticsEngine::Warning, FormatString);
+            (Hint!=NULL) ? diagEngine.Report(Loc, DiagID) << *Hint : diagEngine.Report(Loc, DiagID);
+        }
+
+        
         
         void run(const MatchFinder::MatchResult &Result) {
             
-                if (const ObjCMethodDecl *Mdecl = Result.Nodes.getNodeAs<ObjCMethodDecl>("ObjCMethodDecl")) {
-
-                    JJVisitObjCMethodDecl(Mdecl);
-
-                }
-            
-            if (const ObjCInterfaceDecl *decl = Result.Nodes.getNodeAs<ObjCInterfaceDecl>("ObjCInterfaceDecl")) {
+            if (const ObjCPropertyDecl *propertyDecl = Result.Nodes.getNodeAs<ObjCPropertyDecl>("objcPropertyDecl")) {
                 
-                StringRef className = decl->getName();
+                cout << "objcPropertyDecl -- objcPropertyDecl" << endl;
+                // 存储 Objective-C 类属性
+//                checkPropertyDecl(propertyDecl);
                 
-                char c = className[0];
-                if (isLowercase(c)) {
-                    std::string tempName = className;
-                    cout << tempName << endl;
-                    tempName[0] = toUppercase(c);
-                    StringRef replacement(tempName);
-                    SourceLocation nameStart = decl->getLocation();
-                    SourceLocation nameEnd = nameStart.getLocWithOffset(className.size() - 1);
-                    FixItHint fixItHint = FixItHint::CreateReplacement(SourceRange(nameStart, nameEnd), replacement);
-                    
-                    DiagnosticsEngine &D = ci.getDiagnostics();
-                    int diagID = D.getCustomDiagID(DiagnosticsEngine::Error, "老子警告你：类名不能以小写字母开头");
-                    SourceLocation location = decl->getLocation();
-                    D.Report(location, diagID).AddFixItHint(fixItHint);
-                }
+            } else if (const ObjCInterfaceDecl *interfaceDecl = Result.Nodes.getNodeAs<ObjCInterfaceDecl>("ObjCInterfaceDecl")) {
                 
-                size_t pos = decl->getName().find('_');
-                if (pos != StringRef::npos) {
-                    DiagnosticsEngine &D = ci.getDiagnostics();
-                    SourceLocation loc = decl->getLocation().getLocWithOffset(pos);
-                    
-                    std::string tempName = className;
-                    std::string::iterator end_pos = std::remove(tempName.begin(), tempName.end(), '_');
-                    tempName.erase(end_pos, tempName.end());
-                    StringRef replacement(tempName);
-                    SourceLocation nameStart = decl->getLocation();
-                    SourceLocation nameEnd = nameStart.getLocWithOffset(className.size() - 1);
-                    FixItHint fixItHint = FixItHint::CreateReplacement(SourceRange(nameStart, nameEnd), replacement);
-                    
-                    int diagID = D.getCustomDiagID(DiagnosticsEngine::Error, "老子警告你：类名中不能带有下划线");
-                    D.Report(loc, diagID).AddFixItHint(fixItHint);
-                }
+                cout << "ObjCInterfaceDecl -- ObjCInterfaceDecl" << endl;
+                checkInterfaceDecl(interfaceDecl);
                 
+            } else if (const ObjCMethodDecl *decl = Result.Nodes.getNodeAs<ObjCMethodDecl>("ObjCMethodDecl")) {
+                
+                cout << "ObjCMethodDecl -- ObjCMethodDecl" << endl;
+                checkMethodNameForUppercaseName(decl);
+                
+            } else if (const BinaryOperator *binaryOperator = Result.Nodes.getNodeAs<BinaryOperator>("binaryOperator_modelOfClass")) {
+//                checkAppointedMethod(binaryOperator);
+            } else if (const IfStmt *stmtIf = Result.Nodes.getNodeAs<IfStmt>("ifStmt_empty_then_body")) {
+                SourceLocation location = stmtIf->getIfLoc();
+                diagWaringReport(location, "Don't use empty body in IfStmt", NULL);
+            } else if (const IfStmt *stmtIf = Result.Nodes.getNodeAs<IfStmt>("condition_always_true")) {
+                SourceLocation location = stmtIf->getIfLoc();
+                diagWaringReport(location, "Body will certainly be executed when condition true", NULL);
+            } else if (const IfStmt *stmtIf = Result.Nodes.getNodeAs<IfStmt>("condition_always_false")) {
+                SourceLocation location = stmtIf->getIfLoc();
+                diagWaringReport(location, "Body will never be executed when condition false.", NULL);
             }
+            
+//                if (const ObjCMethodDecl *Mdecl = Result.Nodes.getNodeAs<ObjCMethodDecl>("ObjCMethodDecl")) {
+////                    JJVisitObjCMethodDecl(Mdecl);
+//                }
+            
+           
         }
     };
         
@@ -167,12 +176,19 @@ namespace JJPlugin {
     private:
         MatchFinder matcher;
         JJHandler handler;
-        
+//        CodingStyleASTVisitor visitor;
     public:
         //调用CreateASTConsumer方法后就会加载Consumer里面的方法
         JJASTConsumer(CompilerInstance &ci) :handler(ci) {
             matcher.addMatcher(objcInterfaceDecl().bind("ObjCInterfaceDecl"), &handler);
             matcher.addMatcher(objcMethodDecl().bind("ObjCMethodDecl"), &handler);
+            matcher.addMatcher(objcPropertyDecl().bind("objcPropertyDecl"), &handler);
+            matcher.addMatcher(binaryOperator(hasDescendant(opaqueValueExpr(hasSourceExpression(objcMessageExpr(hasSelector("modelOfClass:"))))),isExpansionInMainFile()).bind("binaryOperator_modelOfClass"), &handler);
+
+//            matcher.addMatcher(ifStmt(isExpansionInMainFile(),hasThen(compoundStmt(statementCountIs(0)))).bind("ifStmt_empty_then_body"), &handler);
+//            matcher.addMatcher(ifStmt(isExpansionInMainFile(),hasCondition(integerLiteral(equals(1)))).bind("condition_always_true"), &handler);
+//            matcher.addMatcher(ifStmt(isExpansionInMainFile(),hasCondition(floatLiteral(equals(0.0)))).bind("condition_always_false"), &handler);
+//            matcher.addMatcher(ifStmt(isExpansionInMainFile(),hasCondition(integerLiteral(equals(0)))).bind("condition_always_false"), &handler);
         }
         
         //遍历完一次语法树就会调用一次下面方法
@@ -188,42 +204,110 @@ namespace JJPlugin {
             return unique_ptr<JJASTConsumer> (new JJASTConsumer(ci));
         }
         
-        bool ParseArgs(const CompilerInstance &ci, const vector<string> &args) {
+        bool ParseArgs(const CompilerInstance &ci, const std::vector<std::string> &args) {
             return true;
         }
     };
 }
 
-namespace CodingStyle {
+
+
+/**
+namespace JJPlugin
+{
     
-    class CodingStyleASTVisitor : public RecursiveASTVisitor<CodingStyleASTVisitor>
+    class JJClassVisitor : public RecursiveASTVisitor<JJClassVisitor>
     {
     private:
         ASTContext *context;
-        string objcClsImpl;
-        bool objcIsInstanceMethod;
-        string objcSelector;
-        string objcMethodSrcCode;
+    public:
+        void setContext(ASTContext &context)
+        {
+            this->context = &context;
+        }
+        
+        bool VisitObjCInterfaceDecl(ObjCInterfaceDecl *declaration)
+        {
+            checkForLowercasedName(declaration);
+            checkForUnderscoreInName(declaration);
+            return true;
+        }
+        
+        void checkForUnderscoreInName(ObjCInterfaceDecl *declaration)
+        {
+            StringRef name = declaration->getName();
+            size_t underscorePos = name.find('_');
+            if (underscorePos != StringRef::npos) {
+                std::string tempName = name;
+                std::string::iterator end_pos = std::remove(tempName.begin(), tempName.end(), '_');
+                tempName.erase(end_pos, tempName.end());
+                StringRef replacement(tempName);
+                
+                SourceLocation nameStart = declaration->getLocation();
+                SourceLocation nameEnd = nameStart.getLocWithOffset(name.size());
+                
+                FixItHint fixItHint = FixItHint::CreateReplacement(SourceRange(nameStart, nameEnd), replacement);
+                
+                DiagnosticsEngine &diagEngine = context->getDiagnostics();
+                unsigned diagID = diagEngine.getCustomDiagID(DiagnosticsEngine::Error, "Class name with `_` forbidden");
+                SourceLocation location = declaration->getLocation().getLocWithOffset(underscorePos);
+                diagEngine.Report(location, diagID).AddFixItHint(fixItHint);
+            }
+        }
+        
+        void checkForLowercasedName(ObjCInterfaceDecl *declaration)
+        {
+            StringRef name = declaration->getName();
+            char c = name[0];
+            if (isLowercase(c)) {
+                std::string tempName = name;
+                tempName[0] = toUppercase(c);
+                StringRef replacement(tempName);
+                
+                SourceLocation nameStart = declaration->getLocation();
+                SourceLocation nameEnd = nameStart.getLocWithOffset(name.size());
+                
+                FixItHint fixItHint = FixItHint::CreateReplacement(SourceRange(nameStart, nameEnd), replacement);
+                
+                DiagnosticsEngine &diagEngine = context->getDiagnostics();
+                unsigned diagID = diagEngine.getCustomDiagID(DiagnosticsEngine::Warning, "Class name should not start with lowercase letter");
+                SourceLocation location = declaration->getLocation();
+                diagEngine.Report(location, diagID).AddFixItHint(fixItHint);
+            }
+        }
     };
     
-    class CodingStyleASTConsumer : public ASTConsumer
-    {
-    private:
-        CodingStyleASTVisitor visitor;
-        void HandleTranslationUnit(ASTContext &context);
-    };
-    
-    class CodingStyleASTAction : public PluginASTAction
+    class JJConsumer : public ASTConsumer
     {
     public:
-        unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &Compiler,llvm::StringRef InFile);
-        bool ParseArgs(const CompilerInstance &CI, const std::vector<std::string>& args);
+        void HandleTranslationUnit(ASTContext &context) {
+            visitor.setContext(context);
+            visitor.TraverseDecl(context.getTranslationUnitDecl());
+        }
+    private:
+        JJClassVisitor visitor;
+    };
+    
+    class JJASTAction : public PluginASTAction
+    {
+    public:
+//        unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &ci, llvm::StringRef InFile){
+//            return unique_ptr<JJConsumer> (new JJConsumer(ci));
+//        }
+        
+        unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &ci, StringRef iFile);
+        
+        bool ParseArgs(const CompilerInstance &CI, const
+                       std::vector<std::string>& args) {
+            return true;
+        }
     };
 }
-
+*/
 
 static FrontendPluginRegistry::Add<JJPlugin::JJASTAction>
 X("JJPlugin", "The JJPlugin is my first clang-plugin.");
 
-static clang::FrontendPluginRegistry::Add<CodingStyle::CodingStyleASTAction>
-Y("ClangCodingStylePlugin", "ClangCodingStylePlugin");
+//static clang::FrontendPluginRegistry::Add<CodingStyle::CodingStyleASTAction>
+//Y("ClangCodingStylePlugin", "ClangCodingStylePlugin");
+
